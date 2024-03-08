094e607141b59ef13596a17212fb6539e952e6aa
tristan-derez <tristan.derez@gmail.com>
2024-03-08 17:26:55 +0100

    create text_node_to_html_node method

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,12 @@
+from htmlnode import LEAFNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -13,6 +22,21 @@ class TextNode:
             )
         
         return False
+    
+    def text_node_to_html_node(self, text_node):
+        if text_node.text_type == text_type_text:
+            return LEAFNode(None, text_node.text)
+        if text_node.text_type == text_type_bold:
+            return LEAFNode("b", text_node.text)
+        if text_node.text_type == text_type_italic:
+            return LEAFNode("i", text_node.text)
+        if text_node.text_type == text_type_code:
+            return LEAFNode("code", text_node.text)
+        if text_node.text_type == text_type_link:
+            return LEAFNode("a", text_node.text, {"href": text_node.url})
+        if text_node.text_type == text_type_image:
+            return LEAFNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
         
     def __repr__(self):
         return f"TextNode({repr(self.text)}, {repr(self.text_type)}, {repr(self.url)})"
