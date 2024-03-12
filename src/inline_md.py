3701c73433f31d6b79b3a92dd1e29dd8962f9c06
tristan-derez <tristan.derez@gmail.com>
2024-03-12 11:21:26 +0100

    create split_nodes_delimiter

---
+++ src/inline_md.py
@@ -0,0 +1,26 @@
+from textnode import (
+    TextNode,
+    text_type_text,
+)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type_text))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    
+    return new_nodes
