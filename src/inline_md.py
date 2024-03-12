ec3a5c13c943d9711e9cc4ded99d9ebac6bfb4e2
tristan-derez <tristan.derez@gmail.com>
2024-03-12 12:49:02 +0100

    create func to split image and link and convert everything to textnodes

--- src/inline_md.py
+++ src/inline_md.py
@@ -3,6 +3,11 @@ import re
 from textnode import (
     TextNode,
     text_type_text,
+    text_type_image,
+    text_type_link,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
 )
 
 
@@ -27,6 +32,65 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
 
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        og_text = node.text
+        images = extract_markdown_images(og_text)
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+        for image in images:
+            sections = og_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(TextNode(image[0], text_type_image, image[1]))
+            og_text = sections[1]
+        if og_text != "":
+            new_nodes.append(TextNode(og_text, text_type_text))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        og_text = node.text
+        links = extract_markdown_links(og_text)
+        for link in links:
+            sections = og_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+            og_text = sections[1]
+        if og_text != "":
+            new_nodes.append(TextNode(og_text, text_type_text))
+
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
 
 
 def extract_markdown_images(text):
