13ca25f23666d62acf4bb26752fe025c7eaee76a
tristan-derez <tristan.derez@gmail.com>
2024-03-12 14:55:51 +0100

    create copy_dir func

---
+++ src/copy_dir.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+
+def copy_directory_folder(src_dir, dest_dir):
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print(f"Copied file: {src_path} to {dest_path}")
+        elif os.path.isdir(src_path):
+            copy_directory_folder(src_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copy_dir import copy_directory_folder
 
 
 def main():
-    print("hello guys")
+    src_dir = "./static"
+    dest_dir = "./public"
+
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+        print(f"Deleted contents of {dest_dir}")
+
+    copy_directory_folder(src_dir, dest_dir)
 
 
 main()
