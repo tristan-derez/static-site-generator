f3a754cc2cbfda1bcc5b74783ab753829bf4f0ee
tristan-derez <tristan.derez@gmail.com>
2024-03-12 11:34:43 +0100

    create extract func for images and links in md

--- src/inline_md.py
+++ src/inline_md.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import (
     TextNode,
     text_type_text,
@@ -24,3 +26,12 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return matches
