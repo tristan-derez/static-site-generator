0fa5e9f093c2973296a912a0e63db581d3e13da8
tristan-derez <tristan.derez@gmail.com>
2024-03-12 11:21:37 +0100

    create test for inline_md

---
+++ src/test_inline_md.py
@@ -0,0 +1,74 @@
+import unittest
+from inline_md import (
+    split_nodes_delimiter,
+)
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+)
+
+
+class TestInlineMD(unittest.TestCase):
+    def test_delimiter_italic(self):
+        node = TextNode("A text with an *italic* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("A text with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+    def test_delimiter_code(self):
+        node = TextNode("A text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("A text with a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+    def test_delimiter_bold(self):
+        node = TextNode("A text with a **bolded** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("A text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+    def test_delimiter_double_bold(self):
+        node = TextNode(
+            "A text with a **bolded** word and **another**", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("A text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word and ", text_type_text),
+                TextNode("another", text_type_bold),
+            ],
+            new_nodes,
+        )
+
+    def test_section_not_closed(self):
+        node = TextNode("A text with a `code block not closed", text_type_text)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "`", text_type_code)
+
+
+if __name__ == "__main__":
+    unittest.main()
